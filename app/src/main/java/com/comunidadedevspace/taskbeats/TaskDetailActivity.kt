6dbae80da31ca83906c84f5c508d904be518742b
Thaines-10 <thainesademir@gmail.com>
2024-04-08 22:06:03 +0200

    Comentario testando 2

--- app/src/main/java/com/comunidadedevspace/taskbeats/TaskDetailActivity.kt
+++ app/src/main/java/com/comunidadedevspace/taskbeats/TaskDetailActivity.kt
@@ -14,7 +14,7 @@ import android.widget.EditText
 import com.google.android.material.snackbar.Snackbar
 
 class TaskDetailActivity : AppCompatActivity() {
-// testando
+// testando 2
    private var task: Task? = null
    private lateinit var btnDone: Button
 
